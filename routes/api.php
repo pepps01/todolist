20f12d85d9d7e28dc033bdecc5b165dcec8793e1
pepps01git statusquit()exit;quit <slpepple01@gmail.com>
2021-11-05 13:00:46 +0100

    Routes Created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::get('/',[TodoController::class,'index']);
+Route::get('{id}',[TodoController::class,'show']);
+Route::post('create-todo',[TodoController::class,'create']);
+Route::get('edit',[TodoController::class,'edit']);
+Route::post('update',[TodoController::class,'update']);
+Route::post('delete',[TodoController::class,'destroy']);
